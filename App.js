e0fc55ee50df4ac24bed9f07114af4de2ea8bbc2
Clément Wds <71884576+Clement-Wds@users.noreply.github.com>
2023-03-29 16:40:03 +0200

    Redux

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import {ThemeProvider} from 'styled-components/native';
-import {theme} from './src/components/styles/theme';
+import { Provider } from 'react-redux';
+
 import Routes from './src/config/routes';
+
+import { store } from "./src/config/store";
+import {theme} from './src/components/styles/theme';
 
 const App = () => {
   return (
+    // <Provider store={store}>
+    //   <ThemeProvider theme={theme}>
+    //     <Routes />
+    //   </ThemeProvider>
+    // </Provider>
     <ThemeProvider theme={theme}>
       <Routes />
     </ThemeProvider>

248f2a33d78b8564f90cb455518d24af6c62efd5
Clément Wds <71884576+Clement-Wds@users.noreply.github.com>
2023-03-30 16:56:35 +0200

    UPDATE Actions Redux

--- src/actions/selectAudio.js
+++ src/actions/selectAudio.js
@@ -1,7 +1,5 @@
-export const SELECT_AUDIO = 'SELECT_AUDIO'
-
-export const selectAudio = () =>({
-
-    type: SELECT_AUDIO
-
-})
+export const SET_TITLE = 'SET_TITLE';
+export const SET_DESCRIPTION = 'SET_DESCRIPTION';
+export const SET_AUDIO_FILE = 'SET_AUDIO_FILE';
+export const SET_ARTISTE = 'SET_ARTISTE';
+export const SET_ALBUM = 'SET_ALBUM';

99131eb7765b42045529a6cb3b6df28fabe97211
clementdufour94 <clement.dufourl@gmail.com>
2023-03-30 22:50:47 +0200

    Add DopList et CheckBox

--- src/screens/addMusique.js
+++ src/screens/addMusique.js
@@ -6,7 +6,6 @@ import {
   View,
   PermissionsAndroid,
 } from 'react-native';
-import {getAuth} from 'firebase/auth';
 import {initializeApp} from 'firebase/app';
 import config from '../../firebase';
 import {useEffect} from 'react';
@@ -18,10 +17,12 @@ import '@react-native-firebase/database';
 import TextInput from '../components/textInput';
 import styled from 'styled-components/native';
 import DocumentPicker from 'react-native-document-picker';
-import { requestMultiple  } from 'react-native-permissions';
-import {getDatabase} from "firebase/database"
-import {ref,set,get, push} from "firebase/database"
-const OPEN_DOCUMENT = 'android.intent.action.OPEN_DOCUMENT';
+import {requestMultiple} from 'react-native-permissions';
+import {getDatabase} from 'firebase/database';
+import {ref, set, get} from 'firebase/database';
+
+import {SelectList} from 'react-native-dropdown-select-list';
+import CheckBox from '@react-native-community/checkbox';
 
 // Initialiser Firebase
 if (!firebase.apps.length) {
@@ -44,22 +45,75 @@ const addMusique = () => {
     requestPermissions();
   }, []);
 
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
   const [audioFile, setAudioFile] = useState(null);
-  const [inputs, setInputs] = useState({name: ''});
+  const [artiste, setArtiste] = useState('');
+  const [album, setAlbum] = useState('');
+  const [selected, setSelected] = useState('');
+  const [isSelected, setSelection] = useState(false);
 
-  const addArtist = async () => {
-    set(ref(db, 'artist/' + inputs.name), {
-      username: inputs,
-    })
-      .then(() => {
-        console.log('Good');
+  const [artists, setArtists] = useState([]);
+  const [selectedArtist, setSelectedArtist] = useState('');
+
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    const artistRef = ref(db, 'artist');
+    get(artistRef)
+      .then(snapshot => {
+        const artists = [];
+        snapshot.forEach(childSnapshot => {
+          const artist = childSnapshot.key;
+          artists.push(artist);
+        });
+        setArtists(artists);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (selectedArtist) {
+      const albumsRef = ref(db, 'album');
+      const q = query(albumsRef, orderByChild('artist').equalTo(selectedArtist));
+      get(q)
+        .then(snapshot => {
+          const albums = [];
+          snapshot.forEach(childSnapshot => {
+            const album = childSnapshot.val();
+            albums.push(album);
+          });
+          setAlbums(albums);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }, [selectedArtist]);
+  const handleArtistChange = event => {
+    const selectedArtist = event.target.value;
+    setSelectedArtist(selectedArtist);
+    // appel de la fonction pour récupérer les albums de l'artiste sélectionné
+    fetchAlbumsByArtist(selectedArtist);
+  };
+
+  const fetchAlbumsByArtist = artist => {
+    const albumsRef = ref(db, 'album');
+    const q = query(albumsRef, orderByChild('artist').equalTo(artist));
+    get(q)
+      .then(snapshot => {
+        const albums = [];
+        snapshot.forEach(childSnapshot => {
+          const album = childSnapshot.val();
+          albums.push(album);
+        });
+        setAlbums(albums);
       })
       .catch(error => {
         console.log(error);
       });
   };
+
 
   // Récupérer une référence de stockage
   const storageRef = firebase.storage().ref();
@@ -88,73 +142,86 @@ const addMusique = () => {
     if (audioFile[0] && audioFile[0].name.endsWith('.mp3')) {
       const fileRef = storageRef.child(`audio/artiste/${audioFile[0].name}`);
       const task = fileRef.putFile(audioFile[0].fileCopyUri);
-      task.on('state_changed', (snapshot) => {
-        // Handle upload progress if needed
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log(`Upload is ${progress}% done`);
-      }, (error) => {
-        // Handle errors during upload if needed
-        console.error(error);
-      }, async () => {
-        // Handle successful upload
-        
-        const downloadURL = await fileRef.getDownloadURL();
-        function cleanFileName(fileName) {
-          return fileName.replace(/[.$#\[\]\/]/g, '_');
-        }
-        
-        const databaseRef = firebase.database().ref(`audios/artiste/${artiste}/${album}`).push();
-const artistRef = ref(db, `artist/${artiste}`);
-const albumRef = ref(db, `artist/${artiste}/${album}`);
-const musicRef = ref(db, `artist/${artiste}/${album}/${cleanFileName(audioFile[0].name)}`);
+      task.on(
+        'state_changed',
+        snapshot => {
+          // Handle upload progress if needed
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        error => {
+          // Handle errors during upload if needed
+          console.error(error);
+        },
+        async () => {
+          // Handle successful upload
 
-Promise.all([
-  get(artistRef),
-  get(albumRef),
-  get(musicRef)
-]).then(results => {
-  const artistExists = results[0].exists();
-  const albumExists = results[1].exists();
-  const musicExists = results[2].exists();
+          const downloadURL = await fileRef.getDownloadURL();
+          function cleanFileName(fileName) {
+            return fileName.replace(/[.$#\[\]\/]/g, '_');
+          }
 
-  if (artistExists && albumExists && musicExists) {
-    console.log("L'artiste, l'album et la musique existent déjà dans la base de données.");
-    return;
-  } else if (artistExists && albumExists && !musicExists) {
-    console.log("L'artiste et l'album existent déjà dans la base de données, mais pas la musique. Ajout de la musique...");
-    return set(musicRef, {name: cleanFileName(audioFile[0].name)});
-  } else if (artistExists && !albumExists) {
-    console.log("L'artiste existe déjà dans la base de données, mais pas l'album ni la musique. Ajout de l'album et de la musique...");
-    return set(albumRef, {
-      [cleanFileName(audioFile[0].name)]: {
-        name: cleanFileName(audioFile[0].name)
-      }
-    });
-  } else {
-    console.log("L'artiste, l'album et la musique n'existent pas dans la base de données. Ajout de l'artiste, de l'album et de la musique...");
-    return set(artistRef, {
-      [album]: {
-        [cleanFileName(audioFile[0].name)]: {
-          name: cleanFileName(audioFile[0].name)
-        }
-      }
-    });
-  }
-}).then(() => {
-  console.log("good");
-}).catch((error) => {
-  console.log(error);
-});
+          const databaseRef = firebase
+            .database()
+            .ref(`audios/artiste/${artiste}/${album}`)
+            .push();
+          const artistRef = ref(db, `artist/${artiste}`);
+          const albumRef = ref(db, `artist/${artiste}/${album}`);
+          const musicRef = ref(
+            db,
+            `artist/${artiste}/${album}/${cleanFileName(audioFile[0].name)}`,
+          );
 
-        
-        console.log("hey");
-        setAudioFile(null);
-        
+          Promise.all([get(artistRef), get(albumRef), get(musicRef)])
+            .then(results => {
+              const artistExists = results[0].exists();
+              const albumExists = results[1].exists();
+              const musicExists = results[2].exists();
 
-        
-        console.log("hey");
-        setAudioFile(null);
-      });
+              if (artistExists && albumExists && musicExists) {
+                console.log(
+                  "L'artiste, l'album et la musique existent déjà dans la base de données.",
+                );
+                return;
+              } else if (artistExists && albumExists && !musicExists) {
+                console.log(
+                  "L'artiste et l'album existent déjà dans la base de données, mais pas la musique. Ajout de la musique...",
+                );
+                return set(musicRef, {name: cleanFileName(audioFile[0].name)});
+              } else if (artistExists && !albumExists) {
+                console.log(
+                  "L'artiste existe déjà dans la base de données, mais pas l'album ni la musique. Ajout de l'album et de la musique...",
+                );
+                return set(albumRef, {
+                  [cleanFileName(audioFile[0].name)]: {
+                    name: cleanFileName(audioFile[0].name),
+                  },
+                });
+              } else {
+                console.log(
+                  "L'artiste, l'album et la musique n'existent pas dans la base de données. Ajout de l'artiste, de l'album et de la musique...",
+                );
+                return set(artistRef, {
+                  [album]: {
+                    [cleanFileName(audioFile[0].name)]: {
+                      name: cleanFileName(audioFile[0].name),
+                    },
+                  },
+                });
+              }
+            })
+            .then(() => {
+              console.log('good');
+            })
+            .catch(error => {
+              console.log(error);
+            });
+
+          console.log('hey');
+          setAudioFile(null);
+        },
+      );
     } else {
       console.log('Invalid audio file uri or file type');
     }
@@ -162,22 +229,43 @@ Promise.all([
 
   return (
     <View>
-      <TextInput
-        placeholder="Titre"
-        value={title}
-        onChangeText={text => setTitle(text)}
+      <SelectList
+        setSelected={val => setSelected(val)}
+        data={artists}
+        save="value"
+        placeholder="Selectionez un artiste"
+        onChange={handleArtistChange}
       />
 
-      <TextInput
-        placeholder="Description"
-        value={description}
-        onChangeText={text => setDescription(text)}
+
+      <Text>L'artiste n'existe pas dans la liste?</Text>
+      <CheckBox
+        disabled={false}
+        value={isSelected}
+        onValueChange={newValue => setSelection(newValue)}
       />
+
+{isSelected ? (
       <TextInput
-        placeholder="Artist name"
-        value={inputs.name}
-        onChangeText={text => setInputs({name: text})}
+        placeholder="Artiste"
+        value={artiste}
+        onChangeText={text => setArtiste(text)}
       />
+      ) : null}
+      <SelectList
+        setSelected={val => setSelected(val)}
+        data={albums}
+        save="value"
+        placeholder="Selectionez un album"
+      />
+     
+      
+        <TextInput
+          placeholder="Album"
+          value={album}
+          onChangeText={text => setAlbum(text)}
+        />
+      
 
       <StyledTouchableOpacity onPress={handleAudioSelect}>
         <StyledText>
@@ -186,7 +274,6 @@ Promise.all([
       </StyledTouchableOpacity>
 
       <Button title="Télécharger" onPress={handleUpload} />
-      <Button title="Télécharger Artiste" onPress={addArtist} />
     </View>
   );
 };

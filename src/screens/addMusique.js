89054f14b780c59d29e61e275389b2dfd08e32a0
clementdufour94 <clement.dufourl@gmail.com>
2023-03-31 15:02:28 +0200

    Ajout photo album

--- src/screens/addMusique.js
+++ src/screens/addMusique.js
@@ -19,7 +19,7 @@ import styled from 'styled-components/native';
 import DocumentPicker from 'react-native-document-picker';
 import {requestMultiple} from 'react-native-permissions';
 import {getDatabase} from 'firebase/database';
-import {ref, set, get,query, orderByChild,equalTo} from 'firebase/database';
+import {ref, set, get, query, orderByChild, equalTo} from 'firebase/database';
 
 import {SelectList} from 'react-native-dropdown-select-list';
 import CheckBox from '@react-native-community/checkbox';
@@ -45,6 +45,8 @@ const addMusique = () => {
   }, []);
 
   const [audioFile, setAudioFile] = useState(null);
+  const [photoFile, setPhotoFile] = useState(null);
+
   const [artiste, setArtiste] = useState('');
   const [album, setAlbum] = useState('');
   const [selected, setSelected] = useState('');
@@ -52,7 +54,6 @@ const addMusique = () => {
 
   const [isSelected, setSelection] = useState(false);
   const [isSelected2, setSelection2] = useState(false);
-
 
   const [artists, setArtists] = useState([]);
   const [selectedArtist, setSelectedArtist] = useState('');
@@ -76,56 +77,58 @@ const addMusique = () => {
   }, []);
 
   useEffect(() => {
-    
     if (selectedArtist) {
       const albumsRef = ref(db, `artist/${selectedArtist}`);
       get(albumsRef)
-        .then((snapshot) => {
+        .then(snapshot => {
           const albums = [];
-          snapshot.forEach((childSnapshot) => {
+          snapshot.forEach(childSnapshot => {
             const album = childSnapshot.key;
             albums.push(album);
           });
           setAlbums(albums);
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
     }
   }, [selectedArtist]);
-  const handleArtistChange = () => {
 
-    const selectedArtist = selected
+  const handleArtistChange = () => {
+    const selectedArtist = selected;
     setSelectedArtist(selectedArtist);
     // appel de la fonction pour récupérer les albums de l'artiste sélectionné
     fetchAlbumsByArtist(selectedArtist);
   };
 
-  const fetchAlbumsByArtist = (artistName) => {
+  const fetchAlbumsByArtist = artistName => {
     const albumsRef = ref(db, `artist/${artistName}/`);
-    console.log("test")
+    console.log('test');
     get(albumsRef)
-      .then((snapshot) => {
+      .then(snapshot => {
         const albums = [];
-        snapshot.forEach((childSnapshot) => {
+        snapshot.forEach(childSnapshot => {
           const album = childSnapshot.val();
           albums.push(album);
         });
-        console.log("les albums sont" && album)
+        console.log('les albums sont' && album);
         setAlbums(albums);
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
       });
   };
-  
-
 
   // Récupérer une référence de stockage
   const storageRef = firebase.storage().ref();
-  useEffect(() => {
-    console.log(audioFile);
-  }, [audioFile]);
+  useEffect(
+    () => {
+      console.log(audioFile);
+      console.log(photoFile);
+    },
+    [audioFile],
+    [photoFile],
+  );
 
   const handleAudioSelect = async () => {
     try {
@@ -144,9 +147,30 @@ const addMusique = () => {
     }
   };
 
+  const handlePhotoSelect = async () => {
+    try {
+      await requestPermissions();
+
+      const result = await DocumentPicker.pick({
+        type: [DocumentPicker.types.images],
+        copyTo: 'documentDirectory',
+        multiple: false,
+        mode: 'open',
+      });
+
+      setPhotoFile(result);
+    } catch (err) {
+      console.log('Error selecting photo file: ', err);
+    }
+  };
+
   const handleUpload = async () => {
     if (audioFile[0] && audioFile[0].name.endsWith('.mp3')) {
-      const fileRef = storageRef.child(`audio/artiste/${audioFile[0].name}`);
+      const fileRef = storageRef.child(
+        `audio/artiste/${isSelected ? artiste : selected}/${album}/${
+          audioFile[0].name
+        }`,
+      );
       const task = fileRef.putFile(audioFile[0].fileCopyUri);
       task.on(
         'state_changed',
@@ -164,22 +188,59 @@ const addMusique = () => {
           // Handle successful upload
 
           const downloadURL = await fileRef.getDownloadURL();
+
+          if (photoFile && isSelected2) {
+            const photoFileRef = storageRef.child(
+              `audio/artiste/${isSelected ? artiste : selected}/${album}/${
+                photoFile[0].name
+              }`,
+            );
+            const photoTask = photoFileRef.putFile(photoFile[0].fileCopyUri);
+            photoTask.on(
+              'state_changed',
+              snapshot => {
+                // Handle upload progress if needed
+                const progress =
+                  (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log(`Upload is ${progress}% done`);
+              },
+              error => {
+                // Handle errors during upload if needed
+                console.error(error);
+              },
+              async () => {
+                // Handle successful upload
+
+                const photoDownloadURL = await photoFileRef.getDownloadURL();
+                // Do something with the photo download URL
+              },
+            );
+          }
           function cleanFileName(fileName) {
             return fileName.replace(/[.$#\[\]\/]/g, '_');
           }
 
-
-          if(isSelected){
-            console.log("check box chéque")
-          }else{
-            console.log("check box pas chéque")
+          if (isSelected) {
+            console.log('check box chéque');
+          } else {
+            console.log('check box pas chéque');
           }
 
-          const artistRef = ref(db, `artist/${isSelected ? artiste :selected}`);
-          const albumRef = ref(db, `artist/${isSelected ? artiste :selected}/${isSelected2 ? album : selected2}`);
+          const artistRef = ref(
+            db,
+            `artist/${isSelected ? artiste : selected}`,
+          );
+          const albumRef = ref(
+            db,
+            `artist/${isSelected ? artiste : selected}/${
+              isSelected2 ? album : selected2
+            }`,
+          );
           const musicRef = ref(
             db,
-            `artist/${isSelected ? artiste :selected}/${isSelected2 ? album : selected2}/${cleanFileName(audioFile[0].name)}`,
+            `artist/${isSelected ? artiste : selected}/${
+              isSelected2 ? album : selected2
+            }/${cleanFileName(audioFile[0].name)}`,
           );
 
           Promise.all([get(artistRef), get(albumRef), get(musicRef)])
@@ -229,6 +290,11 @@ const addMusique = () => {
 
           console.log('hey');
           setAudioFile(null);
+          setPhotoFile(null);
+          setArtiste(null);
+         setAlbum(null);
+         setSelection(null);
+         setSelection2(null);
         },
       );
     } else {
@@ -239,15 +305,14 @@ const addMusique = () => {
   return (
     <View>
       {!isSelected ? (
-      <SelectList
-        setSelected={val => setSelected(val)}
-        data={artists}
-        save="value"
-        placeholder="Selectionez un artiste"
-        onSelect={handleArtistChange}
-      />
+        <SelectList
+          setSelected={val => setSelected(val)}
+          data={artists}
+          save="value"
+          placeholder="Selectionez un artiste"
+          onSelect={handleArtistChange}
+        />
       ) : null}
-
 
       <Text>L'artiste n'existe pas dans la liste?</Text>
       <CheckBox
@@ -256,21 +321,21 @@ const addMusique = () => {
         onValueChange={newValue => setSelection(newValue)}
       />
 
-{isSelected ? (
-      <TextInput
-        placeholder="Artiste"
-        value={artiste}
-        onChangeText={text => setArtiste(text)}
-      />
+      {isSelected ? (
+        <TextInput
+          placeholder="Artiste"
+          value={artiste}
+          onChangeText={text => setArtiste(text)}
+        />
       ) : null}
 
-{!isSelected2 ? (
-      <SelectList
-        setSelected={val => setSelected2(val)}
-        data={albums}
-        save="value"
-        placeholder="Selectionez un album"
-      />
+      {!isSelected2 ? (
+        <SelectList
+          setSelected={val => setSelected2(val)}
+          data={albums}
+          save="value"
+          placeholder="Selectionez un album"
+        />
       ) : null}
       <Text>L'album n'existe pas dans la liste?</Text>
       <CheckBox
@@ -278,18 +343,24 @@ const addMusique = () => {
         value={isSelected2}
         onValueChange={newValue => setSelection2(newValue)}
       />
-     {isSelected2 ? (
-      
+      {isSelected2 ? (
         <TextInput
           placeholder="Album"
           value={album}
           onChangeText={text => setAlbum(text)}
         />
-        ) : null}
+      ) : null}
 
+      {isSelected2 ? (
+        <StyledTouchableOpacity onPress={handlePhotoSelect}>
+          <StyledText>
+            {photoFile ? photoFile[0].name : 'Sélectionner une photo'}
+          </StyledText>
+        </StyledTouchableOpacity>
+      ) : null}
       <StyledTouchableOpacity onPress={handleAudioSelect}>
         <StyledText>
-          {audioFile ? audioFile.name : 'Sélectionner un fichier audio'}
+          {audioFile ? audioFile[0].name : 'Sélectionner un fichier audio'}
         </StyledText>
       </StyledTouchableOpacity>
 
